7648d6c44ac4a2976064566c35afa28dd0389ddb
jrrobedillo <35324250+jrrobedillo@users.noreply.github.com>
2018-01-23 00:11:52 +0800

    Add files via upload

---
+++ Calculator ni Robert.py
@@ -0,0 +1,11 @@
+print ("Teklado ni Robert \nRobert\'s Calculator\n")
+s = float(input("Enter a number: "))
+n = float(input ("Enter another number: "))
+
+print ("Ang suma ay ", s + n)
+print ("Ang bawas ay ", s - n)
+print ("Ang produkto ay ", s * n)
+print ("Ang kusyente ay ", s / n)
+
+
+
