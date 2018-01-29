035db68e6e17b27d4679c4f1efd998c0f1dea884
jrrobedillo <35324250+jrrobedillo@users.noreply.github.com>
2018-01-30 00:01:48 +0800

    Add files via upload

---
+++ Boolean_Comparisons.py
@@ -0,0 +1,13 @@
+print ("Control Structures: Boolean!\n")
+
+s = float(input("Enter the first digit for comparison: "))
+v = float(input("Enter the second digit: "))
+
+print ("\n== is", s == v )
+print ("!= is", s != v )
+print ("> is", s > v )
+print ("< is", s < v )
+print (">= is", s >= v )
+print ("<= is", s <= v )
+
+print("\nTEEHEEEEEE!")
---
+++ If_10_is.py
@@ -0,0 +1,10 @@
+print("\nIf 10 is?\n")
+
+b = input("Input one digit: ")
+
+if 10 > float(b):
+    print("\n10 is greater than " + b)
+if 10 < float(b):
+    print("\n10 smaller than " + b)
+    
+print("\nProgram ended")
